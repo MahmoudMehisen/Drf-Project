c8ded2f64624c002e4bdbd808e2a8b5ef543ae1b
Mahmoud Mehisen <mahmoud.mehisen@gmail.com>
2023-08-28 01:26:09 +0300

    add rest framework

--- ecommerce/core/settings/base.py
+++ ecommerce/core/settings/base.py
@@ -21,6 +21,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # External Packages
+    'rest_framework'
+
+    # Internal Apps
 ]
 
 MIDDLEWARE = [
@@ -93,3 +97,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {}
